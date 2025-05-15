5dc650baf229df5ddcd869739bd1518283fc41b0
Омаров Магомед Русланович <maruomarov@edu.hse.ru>
2025-05-15 22:14:33 +0300

    new db

--- server/database.js
+++ server/database.js
@@ -1,9 +1,14 @@
 // server/database.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: 'game_data.db',
-    logging: false
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 module.exports = sequelize;
